c1ab365a0072dc48f0cceff24ce441bb2062abd0
Ashwin Ramaswami <aramaswamis@gmail.com>
2018-11-19 12:11:08 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,6 @@
-echo "Log in start"
 expo login --non-interactive -u ${EXPO_USERNAME} -p ${EXPO_PASSWORD}
-echo "log in end"
-echo "log out start"
-expo logout
-echo "log out end"
-# expo eject --non-interactive --eject-method expoKit
-# git add -A
-# git commit -m "Eject to ExpoKit"
-# git remote add release git@ssh.dev.azure.com:v3/stanforddaily/StanfordDaily_Mobile/StanfordDaily_Mobile
-# git push -f release master
+expo eject --non-interactive --eject-method expoKit
+git add -A
+git commit -m "Eject to ExpoKit"
+git remote add release git@ssh.dev.azure.com:v3/stanforddaily/StanfordDaily_Mobile/StanfordDaily_Mobile
+git push -f release master
