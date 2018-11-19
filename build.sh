9093a04ce0fc96b69a2e9a3dd4e79521dccfb8c5
Ashwin Ramaswami <aramaswamis@gmail.com>
2018-11-19 12:26:35 -0800

    checkout master

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+git checkout master
 expo login --non-interactive -u ${EXPO_USERNAME} -p ${EXPO_PASSWORD}
 expo eject --non-interactive --eject-method expoKit
 git config --global user.name "Azure DevOps CI Server"
