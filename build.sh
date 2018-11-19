ab38bb1afa84b38c7fdee7158769fe1673c4fceb
Ashwin Ramaswami <aramaswamis@gmail.com>
2018-11-19 12:36:05 -0800

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-git checkout master
-expo login --non-interactive -u ${EXPO_USERNAME} -p ${EXPO_PASSWORD}
-expo eject --non-interactive --eject-method expoKit
-git config --global user.name "Azure DevOps CI Server"
-git config --global user.email "ashwin@stanforddaily.com"
-git add -A
-git commit -m "Eject to ExpoKit"
-git remote add release git@ssh.dev.azure.com:v3/stanforddaily/StanfordDaily_Mobile/StanfordDaily_Mobile
-git push -f release HEAD:master
