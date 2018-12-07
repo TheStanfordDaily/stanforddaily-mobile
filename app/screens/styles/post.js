b94021ee59b3db4882c71e5169d2b9f4dc4102f2
Ashwin Ramaswami <aramaswamis@gmail.com>
2018-12-06 16:22:46 -0800

    Standardize fonts on article page #37

---
+++ app/screens/styles/post.js
@@ -0,0 +1,23 @@
+/*
+ * Styles used in the individual post view (Post.js).
+ */
+import {COLORS, FONTS, ALIGNMENTS, MARGINS, HEIGHTS, FONT_SIZES, STRINGS} from '../../assets/constants.js';
+export default {
+    title: {
+      fontFamily: FONTS.PT_SERIF,
+      fontSize: FONT_SIZES.DEFAULT_LARGE,
+      marginTop: MARGINS.DEFAULT_MARGIN,
+      marginLeft: MARGINS.ARTICLE_SIDES,
+      marginRight: MARGINS.ARTICLE_SIDES,
+    },
+
+    body: {
+      fontFamily: FONTS.PT_SERIF,
+      fontSize: FONT_SIZES.DEFAULT_MEDIUM_SMALL,
+    //   marginTop: MARGINS.DEFAULT_MARGIN,
+    //   marginBottom: MARGINS.ARTICLE_SIDES,
+    //   opacity: 0.80,
+    //   marginLeft: MARGINS.ARTICLE_SIDES,
+    //   marginRight: MARGINS.ARTICLE_SIDES,
+    }
+};
