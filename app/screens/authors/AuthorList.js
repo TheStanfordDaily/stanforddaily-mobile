9839a6d45955c6553dbd22e7d2b0d2c15ab2b269
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-01-05 15:51:20 -0800

    Re-implement nestedlistview to get AuthorList to work on Android #75

--- app/screens/authors/AuthorList.js
+++ app/screens/authors/AuthorList.js
@@ -4,14 +4,15 @@ Each time to run again: call "npm run ios"
 */
 'use strict';
 import React, { Component } from 'react';
-import NestedListView, { NestedRow } from 'react-native-nested-listview';
 import {
   View,
   StatusBar,
   ScrollView,
   Dimensions,
   Text,
-  Image
+  Image,
+  SectionList,
+  TouchableOpacity
 } from 'react-native';
 
 import {
@@ -21,57 +22,19 @@ import {
   STRINGS,
   DEFAULT_IMAGE
 } from '../../assets/constants';
+import NestedListView from "./NestedListView";
 
 import Header from '../common/header';
 
 const { width, height } = Dimensions.get('window'); //Dimensions of the current device screen
 
-const styles = {
-  header: {
-    borderRadius: 8,
-    borderBottomWidth: 3,
-    borderBottomColor: COLORS.LIGHT_GRAY,
-  },
-  headerText: {
-    fontSize: FONT_SIZES.DEFAULT_MEDIUM,
-    fontFamily: FONTS.PT_SERIF_BOLD,
-    top: 8,
-  },
-  author: {
-    borderRadius: 10,
-    borderBottomColor: COLORS.LIGHT_GRAY,
-    borderBottomWidth: 1,
-  },
-  authorText: {
-    fontSize: FONT_SIZES.DEFAULT_SMALL_MEDIUM,
-    fontFamily: FONTS.PT_SERIF,
-    left: 36,
-    top: -11,
-  },
-  image: {
-    marginLeft: -8, 
-    marginTop: 8.5, 
-    width: 34, 
-    height: 34,
-    borderRadius: 17,
-    borderBottomWidth: 1,
-    overflow: "hidden"
-  }
-}
 
-class Post extends Component {
+class AuthorList extends Component {
   constructor(props) {
     super(props);
     this.state = {
       data: [],
-      post: {},
-      width: width <= height ? width : height,
-      height: Dimensions.get('window').height,
     }
-    Dimensions.addEventListener('change', () => {
-      const { width, height } = Dimensions.get('window')
-      this.setState({ width: width <= height ? width : height, height: height });
-    });
   }
 
   //Once components load, load data. All data is passed down from previous screen
@@ -83,8 +46,11 @@ class Post extends Component {
   fetchData() {
     fetch(STRINGS.DAILY_URL + "wp-json/tsd/v1/authors/")
       .then(e => e.json()) //convert to json
-      .then(e => {
-        this.setState({ data: e });
+      .then(data => {
+        for (let section of data) {
+          section.data = section.members.filter(member => member && member.name && member.name.trim());
+        }
+        this.setState({ data: data });
       })
   }
 
@@ -101,36 +67,11 @@ class Post extends Component {
           />
           <ScrollView style={{ flex: 1, flexDirection: "row", backgroundColor: "white" }}
             contentContainerStyle={{ flexGrow: 1, justifyContent: 'space-between' }}>
-
-            <NestedListView
-              data={this.state.data}
-              getChildrenName={(node) => 'members'} //children of the categories
-              onNodePressed={
-                (node) => node.id && this.props.navigation.navigate("AuthorDetail", { id: node.id })
-              }
-              renderNode={(node, level) => ((node.name && node.name.trim()) ?
-                <NestedRow
-                  level={level}
-                  style={level == 1 ? styles.header : styles.author}>
-                  <View style={{ flex: 1 }}>
-                  {level != 1 &&
-                      <View
-                        style={{ flex: 1 }}>
-                      <Image
-                          style={ styles.image }
-                          source={{ uri: node.profileImage || DEFAULT_IMAGE }}
-                        />
-                      </View>}
-                    <View
-                      style={{ flex: 1 }}>
-                      <Text style={level == 1 ? styles.headerText : styles.authorText}>
-                      {node.name}
-                      </Text>
-                    </View>
-                  </View>
-                </NestedRow> : <View />
-              )}
-            />
+            {this.state.data.length ?
+              <NestedListView
+                data={this.state.data}
+                navigate={id => this.props.navigation.navigate("AuthorDetail", { id: id })}
+              /> : <View />}
           </ScrollView>
         </View>
       </View>
@@ -138,4 +79,4 @@ class Post extends Component {
   }
 };
 
-export default Post;
+export default AuthorList;
