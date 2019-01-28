e1224177c1c70a0a5e274239e79fc082654d0752
shanaeh <shanaeh@stanford.edu>
2019-01-27 19:59:59 -0800

    Minor stylistic changes. Note that constants has changed the STRINGS.DAILY_URL to staging, and that has reverted the authors page to the old staging data. Just something to remember when merging!

--- app/screens/map/Map.js
+++ app/screens/map/Map.js
@@ -16,6 +16,7 @@ import {
 } from '../../assets/constants';
 
 import { MapView } from 'expo'; //takes place of react-native-maps
+import Header from '../common/header';
 
 function randomColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
@@ -35,8 +36,6 @@ export default class App extends React.Component {
       //Get current location
       myPosition: null,
 
-      //temporarily hardcoded for now; will change to get info from backend
-      //Also so that description includes link to the article itself
       markers: [],
       // markers: [
       //   {
@@ -46,20 +45,6 @@ export default class App extends React.Component {
       //     description: "Where the magic happens! :)",
       //     color: randomColor(),
       //     id: 1
-      //   },
-      //   {
-      //     coordinate: {latitude: 37.429720, longitude: -122.170630},
-      //     title: "testing2222",
-      //     description: "testing",
-      //     color: randomColor(),
-      //     id: 2
-      //   },
-      //   {
-      //     coordinate: {latitude: 37.420000, longitude: -122.171730},
-      //     title: "another test",
-      //     description: "testing",
-      //     color: randomColor(),
-      //     id: 3
       //   },
       // ],
     };
@@ -94,18 +79,19 @@ export default class App extends React.Component {
   render() {
     return (
       <View style = {styles.container}>
+      <Header ref='postHeader'/>
       <MapView
         style={styles.map}
         initialRegion={this.state.initialRegion}
       >
       {this.state.markers.map(marker => (
           <MapView.Marker
-            //key={marker.id}
-            coordinate={{latitude: marker.coordinates[0], longitude: marker.coordinates[1]}}
+            key={marker.id}
+            coordinate={{latitude: marker.coordinates[0], 
+              longitude: marker.coordinates[1]}}
             title={marker.name}
             description={marker.description}
-            //pinColor= randomColor()
-            //pinColor={marker.color}
+            pinColor = {randomColor()}
           />
       ))}
       </MapView>
@@ -130,105 +116,3 @@ var styles = StyleSheet.create({
       bottom: 0,
     }
 });
-/*
-        {this.state.markers.map(markers => (//List of markers to render
-          <Marker
-            coordinate={marker.latlng}
-            title={marker.title}
-            description={marker.description}
-          />
-        ))}
-
-        //Just place 1 coordinate hard code
-      <MapView.Marker
-        coordinate={{latitude: 37.425690, longitude: -122.170600}}
-        title={"The Stanford Daily Building"}
-        description={"Where the magic happens! :)"}
-      />
-*/
-//Goal: add geotag locations onto the map
-
-/*
-Note: using the post.js as a template for this page; hoping it mostly works!
-Each time to run again: call "npm run ios"
-*/
-/*
-'use strict';
-import React, { Component } from 'react';
-import {
-  View,
-  StatusBar,
-  ScrollView,
-  Dimensions,
-  Text,
-  Image,
-  SectionList,
-  TouchableOpacity
-} from 'react-native';
-
-import {
-  COLORS,
-  FONTS,
-  FONT_SIZES,
-  STRINGS,
-  DEFAULT_IMAGE
-} from '../../assets/constants';
-import NestedListView from "./NestedListView";
-
-import Header from '../common/header';
-
-const { width, height } = Dimensions.get('window'); //Dimensions of the current device screen
-
-
-class AuthorList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: [],
-    }
-  }
-
-  //Once components load, load data. All data is passed down from previous screen
-  componentDidMount() {
-    this.fetchData();
-  }
-
-  //Gets data and makes it look as expected
-  fetchData() {
-    fetch(STRINGS.DAILY_URL + "wp-json/tsd/v1/authors/")
-      .then(e => e.json()) //convert to json
-      .then(data => {
-        for (let section of data) {
-          section.data = section.members.filter(member => member && member.name && member.name.trim());
-        }
-        this.setState({ data: data });
-      })
-  }
-
-  createMarkup(text) {
-    return text;
-  }
-  render() {
-    return (
-      <View style={{ flex: 1 }}>
-        <Header ref='postHeader' postID={this.state.id} />
-        <View style={{ flex: 1, alignItems: 'center' }}>
-          <StatusBar
-            barStyle="light-content"
-          />
-          <ScrollView style={{ flex: 1, flexDirection: "row", backgroundColor: "white" }}
-            contentContainerStyle={{ flexGrow: 1, justifyContent: 'space-between' }}>
-            {this.state.data.length ?
-              <NestedListView
-                data={this.state.data}
-                navigate={id => this.props.navigation.navigate("AuthorDetail", { id: id })}
-              /> : <View />}
-          </ScrollView>
-        </View>
-      </View>
-    );
-  }
-};
-
-export default AuthorList;
-*/
