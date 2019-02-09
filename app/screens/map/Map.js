bc48ada4b3a2eb0a8a06f91bff3e9060aacb8824
Yifei He <yifei@hesyifei.com>
2019-02-08 19:04:17 -0800

    improve map view posts list animation

--- app/screens/map/Map.js
+++ app/screens/map/Map.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Alert, Image, AppRegistry, TouchableHighlight, TouchableOpacity, ScrollView, StyleSheet, View, Text, Dimensions } from 'react-native';
+import { Animated, ListView, Image, TouchableHighlight, TouchableOpacity, ScrollView, StyleSheet, View, Text, Dimensions } from 'react-native';
 import { SearchBar } from 'react-native-elements';
 import _ from "lodash";
 import HTML from '../../HTML';
@@ -12,10 +12,14 @@ const LATITUDE = 37.4275;
 const LONGITUDE = -122.1697;
 const LATITUDE_DELTA = 0.0300;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+const initialPostsViewHeight = 300;
 
 export default class MapExample extends Component {
   constructor() {
     super();
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
     this.state = {
       shown: false,
       posts: null,
@@ -26,12 +30,15 @@ export default class MapExample extends Component {
         longitude: LONGITUDE,
         latitudeDelta: LATITUDE_DELTA,
         longitudeDelta: LONGITUDE_DELTA,
-      }
+      },
+      scrollY: new Animated.Value(0),
+      dataSource: ds.cloneWithRows([]),
+      ViewScale: new Animated.Value(300),
     };
   }
 
 
-  toggleStatus() {
+  toggleShownStatus() {
     this.setState({
       shown: !this.state.shown
     });
@@ -89,11 +96,16 @@ export default class MapExample extends Component {
 
   fetchLocation(locationID) {
 
+    let ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2,
+    });
+
     // Todo: post pagination
     fetch(STRINGS.DAILY_URL + "wp-json/tsd/v1/locations/" + locationID + "/posts?").then(e => {
       return e.json();
     }).then(e => {
-      this.setState({ posts: e })
+      this.setState({ dataSource: ds.cloneWithRows(e) });
+      console.log(this.state.dataSource);
     })
   }
 
@@ -112,26 +124,142 @@ export default class MapExample extends Component {
   }
 
 
+  renderRow(post) {
+    console.log("yeppppp", post);
+    if (post.id) {
+      return (
+        <View key={post.id} style={{ flex: 0.1, margin: 2, backgroundColor: "white", borderTopWidth: 1, borderTopColor: "gray", flexDirection: "column" }}>
+          <View style={{ flex: 1, marginTop: 1, backgroundColor: "white", flexDirection: "row" }}>
+            <View style={{ flex: 2, padding: 7, aspectRatio: 3 / 2 }}>
+              <Image
+                style={{
+                  flex: 1,
+                  alignSelf: 'center',
+                  width: '100%',
+                  height: undefined
+                }}
+                source={{ uri: _.get(post, "_embedded.wp:featuredmedia.0.media_details.sizes.thumbnail.source_url", DEFAULT_IMAGE) }}
+              />
+            </View>
+            <View style={{ flex: 3, paddingTop: 20, paddingBottom: 10, paddingLeft: 5, paddingRight: 10 }}>
+              <TouchableHighlight onPress={() => this.props.navigation.navigate(STRINGS.POST, { postID: post.id })}>
+                <HTML baseFontStyle={{ fontSize: 16, fontFamily: "Hoefler Text" }} html={post.title.rendered} />
+              </TouchableHighlight>
+              <Text style={{ fontSize: 12, fontFamily: "Helvetica-Bold", color: 'gray', paddingTop: 5 }}>
+                {new Date(post.date).toLocaleDateString()}
+              </Text>
+            </View>
+          </View>
+        </View>
+      );
+    }
+    return (<View><Text>Loading</Text></View>);
+  }
+
+
   render() {
+    var headMov = this.state.scrollY.interpolate({
+      inputRange: [0, initialPostsViewHeight, height],
+      outputRange: [0, -initialPostsViewHeight, -height]
+    });
+    var headColor = this.state.scrollY.interpolate({
+      inputRange: [0, 100],
+      outputRange: ["green", "blue"]
+    });
     return (
       <View style={{ flex: 1 }}>
+        {/*<ListView
+          dataSource={this.state.dataSource}
+          renderRow={this.renderRow.bind(this)}
+          renderScrollComponent={this.renderScroll.bind(this)}
+          style={{backgroundColor: "red"}}
+        />*/}
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this.renderRow.bind(this)}
+          renderScrollComponent={this.renderScroll.bind(this)}
+          withSections={false}
+          enableEmptySections={true}
+          stickySectionHeadersEnable={true}
+          renderSectionHeader={() => {
+            return (
+              <View style={{
+                flex: 1,
+                alignContent: "center",
+                flexDirection: "row",
+                backgroundColor: "red"
+              }}
+              >
 
-        <View style={{ flex: 1 }}>
-          <View>
-            <SearchBar style={{ position: "fixed", flex: 1 }}
-              onChangeText={this.handleLocationInput}
-              //onClearText={someMethod}
+                <View style={{ flex: 2 }}>
+                  <Image
+                    style={{ marginTop: 7, width: 16, height: 35, alignSelf: "center" }}
+                    source={require('../../media/pin.png')}
+                  />
+                </View>
 
-              showLoading={true}
-              lightTheme
-              platform="default"
-              round={true}
-              cancelButtonTitle="Cancel"
-              placeholder="Search" />
-          </View>
+                <View style={{ flex: 7, justifyContent: "center" }}>
+                  <Text style={{
+                    flex: 1,
+                    paddingTop: 12,
+                    fontSize: 16,
+                    fontFamily: "Hoefler Text",
+                    fontWeight: "bold",
+                    alignContent: "center",
+                  }}>
+                    Articles related to: {"\n"}
+                    {this.state.name}
+                  </Text>
+                </View>
 
+                <View style={{ flex: 3, justifyContent: "center" }}>
+                  <TouchableHighlight style={{
+                    height: 30,
+                    width: 30,
+                    margin: 2,
+                    borderRadius: 100,
+                    alignSelf: "center",
+                    backgroundColor: "grey"
+                  }}>
+                    <TouchableOpacity
+                      onPress={() => {
+                        // Alert.alert('You will now receive push notifications alerting you about new articles related to the Rodin Sculpture Garden!')
+                        this.closePostsView();
+                      }}>
+                      <Text style={{
+                        margin: 5,
+                        fontSize: 15,
+                        color: "white",
+                        alignSelf: "center"
+                      }}>
+                        X
+                    </Text>
+                    </TouchableOpacity>
+                  </TouchableHighlight>
+                </View>
+              </View>
+            );
+          }}
+        >
+
+
+
+        </ListView>
+
+
+        <Animated.View
+          style={{
+            position: "absolute",
+            height: '100%',
+            width: width,
+            top: 0,
+            justifyContent: "flex-end",
+            flexDirection: "column",
+            transform: [{ translateY: headMov }]
+          }}
+        >
           <MapView
-            style={styles.container}
+            style={{ bottom: 0, width: width, height: '100%' }}
             showsUserLocation={true}
             //followsUserLocation = {true}
             showsCompass={true}
@@ -154,14 +282,23 @@ export default class MapExample extends Component {
                 title={marker.name}
 
                 description={marker.description}
-                onPress={() =>
-                  {
-                    this.toggleStatus()
-                    this.setState({name: marker.name})
-                    this.fetchLocation(marker.id);
-                  }
+                onPress={() => {
+                  /*this.toggleStatus()
+                  this.setState({ name: marker.name })
+                  this.fetchLocation(marker.id);*/
+                  this.closePostsView();
+                }}
+                onCalloutPress={() => {
+                  /*this.toggleStatus()
+                  this.setState({ name: marker.name })
+                  this.fetchLocation(marker.id);*/
+
+                  this.setState({ name: marker.name });
+                  this.fetchLocation(marker.id);
+                  this.toggle();
                 }
-                >
+                }
+              >
                 {/* https://stackoverflow.com/a/33471432/2603230 */}
                 <View style={[styles.markerBackground, { backgroundColor: marker.iconBackgroundColor, borderColor: marker.iconBorderColor }]}>
                   <MaterialCommunityIcons name={marker.icon} size={20} color={marker.iconColor} style={styles.markerInnerIcon} />
@@ -169,129 +306,71 @@ export default class MapExample extends Component {
               </MapView.Marker>
             ))}
           </MapView>
-        </View>
-
-
-
-
-
-        {this.state.shown ?
-          <View style={{
-            flex: 1,
-            backgroundColor: "white",
-            borderTopLeftRadius: 20,
-            borderTopRightRadius: 20
-          }}>
-
-            <View style={{
-              marginTop: 5,
-              flex: 0.2,
-              alignContent: "center",
-              flexDirection: "row"
-            }}>
-
-              <View style={{ flex: 2 }}>
-                <Image
-                  style={{ marginTop: 7, width: 16, height: 35, alignSelf: "center" }}
-                  source={require('../../media/pin.png')}
-                />
-              </View>
-
-              <View style={{ flex: 7, justifyContent: "center" }}>
-                <Text style={{
-                  flex: 1,
-                  paddingTop: 12,
-                  fontSize: 16,
-                  fontFamily: "Hoefler Text",
-                  fontWeight: "bold",
-                  alignContent: "center",
-                }}>
-                  Articles related to: {"\n"}
-                  {this.state.name}
-              </Text>
-              </View>
-
-              <View style = {{flex: 3, justifyContent: "center"}}>
-              <TouchableHighlight style={{
-                height: 30,
-                width: 30,
-                margin: 2,
-                borderRadius: 100,
-                alignSelf: "center",
-                backgroundColor: "grey"
-              }}>
-                <TouchableOpacity
-                  onPress={() => {
-                    // Alert.alert('You will now receive push notifications alerting you about new articles related to the Rodin Sculpture Garden!')
-                    this.toggleStatus();
-                  }}>
-                  <Text style={{
-                    margin: 5,
-                    fontSize: 15,
-                    color: "white",
-                    alignSelf: "center"
-                  }}>
-                    X
-                  </Text>
-                </TouchableOpacity>
-              </TouchableHighlight>
-              </View>
-
-
-
-
-
-            </View>
-
-
-            <ScrollView showsVerticalScrollIndicator={true} style={{ flex: 1, flexDirection: "column" }}
-              contentContainerStyle={{ flexGrow: 1, justifyContent: 'space-between' }}>
-
-              {this.state.posts && this.state.posts.map(post => <View key={post.id} style={{ flex: 0.1, margin: 2, backgroundColor: "white", borderTopWidth: 1, borderTopColor: "gray", flexDirection: "column" }}>
-
-                <View style={{ flex: 1, marginTop: 1, backgroundColor: "white", flexDirection: "row" }}>
-                  <View style={{ flex: 2, padding: 7, aspectRatio: 3 / 2 }}>
-                    <Image
-                      style={{
-                        flex: 1,
-                        alignSelf: 'center',
-                        width: '100%',
-                        height: undefined
-                      }}
-                      source={{ uri: _.get(post, "_embedded.wp:featuredmedia.0.media_details.sizes.thumbnail.source_url", DEFAULT_IMAGE) }}
-                    />
-                  </View>
-                  <View style={{ flex: 3, paddingTop: 20, paddingBottom: 10, paddingLeft: 5, paddingRight: 10 }}>
-                    <TouchableHighlight onPress={() => this.props.navigation.navigate(STRINGS.POST, { postID: post.id })}>
-                      <HTML baseFontStyle={{ fontSize: 16, fontFamily: "Hoefler Text" }} html={post.title.rendered} />
-                    </TouchableHighlight>
-                    <Text style={{ fontSize: 12, fontFamily: "Helvetica-Bold", color: 'gray', paddingTop: 5 }}>
-                      {new Date(post.date).toLocaleDateString()}
-                    </Text>
-                  </View>
-                </View>
-
-              </View>
-              )}
-
-            </ScrollView>
-          </View>
-
-          : <View></View>}
-
+        </Animated.View>
       </View>
+    );
+  }
 
+  toggle() {
+    if (!this.state.shown) {
+      this.toggleShownStatus();
+      Animated.timing(                    // Animate over time
+        this.state.scrollY,             // The animated value to drive, this would be a new Animated.Value(0) object.
+        {
+          toValue: initialPostsViewHeight,                   // Animate the value
+          duration: 500,                 // Make it take a while
+        }
+      ).start(() => { this.state.scrollY.extractOffset(); });
+    }
+  }
 
+  closePostsView() {
+    if (this.state.shown) {
+      this.state.scrollY.flattenOffset();
 
+      Animated.timing(                    // Animate over time
+        this.state.scrollY,             // The animated value to drive, this would be a new Animated.Value(0) object.
+        {
+          toValue: 0,                   // Animate the value
+          duration: 500,                 // Make it take a while
+        }
+      ).start(() => { this.toggleShownStatus(); });
+    }
+  }
+
+  _handleScroll(e) {
+    console.log(e.nativeEvent.contentOffset.y, "test");
+    console.log(height);
+  }
+
+  renderScroll(props) {
+    console.log("haha");
+    return (
+      <Animated.ScrollView
+        {...props}
+        scrollEventThrottle={16}
+
+        contentContainerStyle={{
+          paddingTop: height - 125 - initialPostsViewHeight,
+        }}
+
+        // Declarative API for animations ->
+        onScroll={Animated.event(
+          [
+            {
+              nativeEvent: { contentOffset: { y: this.state.scrollY } }
+            }
+          ],
+          { listener: this._handleScroll.bind(this) },
+          {
+            useNativeDriver: true // <- Native Driver used for animated events
+          }
+        )}
+      />
     );
   }
 }
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    height: '100%',
-    width: '100%',
-  },
   markerBackground: {
     width: 40,
     height: 40,
