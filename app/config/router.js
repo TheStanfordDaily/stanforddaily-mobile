7c70d71ade576ba597ec4240a77261a7e01bcd69
Yifei He <yifei@hesyifei.com>
2019-02-08 22:22:27 -0800

    update daily icon in navigation bar

--- app/config/router.js
+++ app/config/router.js
@@ -124,7 +124,7 @@ export const Tabs = createBottomTabNavigator({
       tabBarLabel: 'Daily News',
       tabBarIcon: ({ tintColor }) => <Image
         source={require('../media/Daily.png')}
-        style={{tintColor: tintColor, width: 30, height: 27, marginTop: 5}}
+        style={{tintColor: tintColor, width: 20, height: 20, marginTop: 7}}
       />,
       // tabBarOnPress: (scene, jumpToIndex) => {
       //     Amplitude.logEvent(STRINGS.SWITCHED_SCREEN, {NewScreen: STRINGS.NEWS});
