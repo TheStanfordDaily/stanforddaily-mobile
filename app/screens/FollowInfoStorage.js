bf3c5ce2294d6fc132a8110a7bbe2d78ac0b0051
Alexandra Tsai <alextsai@Alexandras-MacBook-Pro-8.local>
2019-02-20 11:13:03 -0800

    added functions to follow locations

--- app/screens/FollowInfoStorage.js
+++ app/screens/FollowInfoStorage.js
@@ -1,41 +1,62 @@
 import {AsyncStorage} from "react-native"
 
-async function followCategory(category_id) {
+export async function followCategory(category_id) {
     categories_followed = JSON.parse(await AsyncStorage.getItem('categories_followed') || "[]"); // store an array
     categories_followed.push(category_id);                                                       // add category_id to categories_followed
     return await AsyncStorage.setItem('categories_followed', JSON.stringify(categories_followed));
 }
 
-async function unfollowCategory(category_id) {
+export async function unfollowCategory(category_id) {
     categories_followed = JSON.parse(await AsyncStorage.getItem('categories_followed') || "[]"); 
     var index = categories_followed.indexOf(category_id);
     if (index !== -1) categories_followed.splice(index);                                     // remove category_id from list
     return await AsyncStorage.setItem('categories_followed', JSON.stringify(categories_followed));
 }
 
-async function followAuthor(author_id) {
+export async function followAuthor(author_id) {
     authors_followed = JSON.parse(await AsyncStorage.getItem('authors_followed') || "[]");  
     authors_followed.push(author_id);                                                        // add author_id to authors_followed
     return await AsyncStorage.setItem('authors_followed', JSON.stringify(authors_followed));
 }
 
-async function unfollowAuthor(author_id) {
+export async function unfollowAuthor(author_id) {
     authors_followed = JSON.parse(await AsyncStorage.getItem('authors_followed') || "[]"); 
     var index = authors_followed.indexOf(author_id);
     if (index !== -1) authors_followed.splice(index);                                       // remove author_id from list
     return await AsyncStorage.setItem('authors_followed', JSON.stringify(authors_followed));
 }
 
-async function isFollowingCategory(category_id) {
+export async function followLocation(location_id) {
+    locations_followed = JSON.parse(await AsyncStorage.getItem('locations_followed') || "[]");  
+    locations_followed.push(location_id);                                                        
+    return await AsyncStorage.setItem('locations_followed', JSON.stringify(locations_followed));
+}
+
+export async function unfollowLocation(location_id) {
+    locations_followed = JSON.parse(await AsyncStorage.getItem('locations_followed') || "[]"); 
+    var index = locations_followed.indexOf(location_id);
+    if (index !== -1) locations_followed.splice(index);                                       
+    return await AsyncStorage.setItem('locations_followed', JSON.stringify(locations_followed));
+}
+
+export async function isFollowingCategory(category_id) {
     categories_followed = JSON.parse(await AsyncStorage.getItem('categories_followed') || "[]");
     var index = categories_followed.indexOf(category_id);
     if (index !== -1) return true;
     else return false;
 }
 
-async function isFollowingAuthor(author_id) {
+export async function isFollowingAuthor(author_id) {
     authors_followed = JSON.parse(await AsyncStorage.getItem('authors_followed') || "[]");
     var index = authors_followed.indexOf(author_id);
     if (index !== -1) return true;
     else return false;
 }
+
+export async function isFollowingLocation(location_id) {
+    locations_followed = JSON.parse(await AsyncStorage.getItem('locations_followed') || "[]");
+    var index = locations_followed.indexOf(location_id);
+    if (index !== -1) return true;
+    else return false;
+}
+
