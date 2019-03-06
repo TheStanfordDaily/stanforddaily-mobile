57fd438340d3ac4eae6220283e0f8d82c84d5e86
Alexandra Tsai <alextsai@DN0a23231e.SUNet>
2019-03-06 09:18:21 -0800

    added functions to update backend

--- app/screens/FollowInfoStorage.js
+++ app/screens/FollowInfoStorage.js
@@ -8,83 +8,124 @@ export async function setToken(token) {
     return await AsyncStorage.setItem('token', token);
 }
 
+export async function getCategoriesFollowed() {
+    return JSON.parse(await AsyncStorage.getItem('categories_followed') || "[]");
+}
+
+export async function getAuthorsFollowed() {
+    return JSON.parse(await AsyncStorage.getItem('authors_followed') || "[]");
+}
+
+export async function getLocationsFollowed() {
+    return JSON.parse(await AsyncStorage.getItem('locations_followed') || "[]");
+}
+
+export async function getNotificationSettings() {
+    JSON.parse(await AsyncStorage.getItem('notification_settings') || "[]");
+}
+
 export async function followCategory(category_id) {
-    categories_followed = JSON.parse(await AsyncStorage.getItem('categories_followed') || "[]"); // store an array
+    categories_followed = getCategoriesFollowed(); // store an array
     categories_followed.push(category_id);                                                       // add category_id to categories_followed
+    updateBackend();
     return await AsyncStorage.setItem('categories_followed', JSON.stringify(categories_followed));
 }
 
 export async function unfollowCategory(category_id) {
-    categories_followed = JSON.parse(await AsyncStorage.getItem('categories_followed') || "[]"); 
+    categories_followed = getCategoriesFollowed(); 
     var index = categories_followed.indexOf(category_id);
     if (index !== -1) categories_followed.splice(index);                                     // remove category_id from list
+    updateBackend();
     return await AsyncStorage.setItem('categories_followed', JSON.stringify(categories_followed));
 }
 
 export async function followAuthor(author_id) {
-    authors_followed = JSON.parse(await AsyncStorage.getItem('authors_followed') || "[]");  
+    authors_followed = getAuthorsFollowed();  
     authors_followed.push(author_id);                                                        // add author_id to authors_followed
+    updateBackend();
     return await AsyncStorage.setItem('authors_followed', JSON.stringify(authors_followed));
 }
 
 export async function unfollowAuthor(author_id) {
-    authors_followed = JSON.parse(await AsyncStorage.getItem('authors_followed') || "[]"); 
+    authors_followed = getAuthorsFollowed(); 
     var index = authors_followed.indexOf(author_id);
     if (index !== -1) authors_followed.splice(index);                                       // remove author_id from list
+    updateBackend();
     return await AsyncStorage.setItem('authors_followed', JSON.stringify(authors_followed));
 }
 
 export async function followLocation(location_id) {
-    locations_followed = JSON.parse(await AsyncStorage.getItem('locations_followed') || "[]");  
+    locations_followed = getLocationsFollowed();  
     locations_followed.push(location_id);                                                        
+    updateBackend();
     return await AsyncStorage.setItem('locations_followed', JSON.stringify(locations_followed));
 }
 
 export async function unfollowLocation(location_id) {
-    locations_followed = JSON.parse(await AsyncStorage.getItem('locations_followed') || "[]"); 
+    locations_followed = getLocationsFollowed(); 
     var index = locations_followed.indexOf(location_id);
     if (index !== -1) locations_followed.splice(index);                                       
+    updateBackend();
     return await AsyncStorage.setItem('locations_followed', JSON.stringify(locations_followed));
 }
 
 export async function isFollowingCategory(category_id) {
-    categories_followed = JSON.parse(await AsyncStorage.getItem('categories_followed') || "[]");
+    categories_followed = getCategoriesFollowed()
     var index = categories_followed.indexOf(category_id);
     if (index !== -1) return true;
     else return false;
 }
 
 export async function isFollowingAuthor(author_id) {
-    authors_followed = JSON.parse(await AsyncStorage.getItem('authors_followed') || "[]");
+    authors_followed = getAuthorsFollowed();
     var index = authors_followed.indexOf(author_id);
     if (index !== -1) return true;
     else return false;
 }
 
 export async function isFollowingLocation(location_id) {
-    locations_followed = JSON.parse(await AsyncStorage.getItem('locations_followed') || "[]");
+    locations_followed = getLocationsFollowed();
     var index = locations_followed.indexOf(location_id);
     if (index !== -1) return true;
     else return false;
 }
 
 export async function addNotificationSetting(notification_id) {
-    notification_settings = JSON.parse(await AsyncStorage.getItem('notification_settings') || "[]");  
+    notification_settings = getNotificationSettings();  
     notification_settings.push(notification_id);                                                        
+    updateBackend();
     return await AsyncStorage.setItem('notification_settings', JSON.stringify(notification_settings));
 }
 
 export async function removeNotificationSetting(notification_id) {
-    notification_settings = JSON.parse(await AsyncStorage.getItem('notification_settings') || "[]"); 
+    notification_settings = getNotificationSettings(); 
     var index = notification_settings.indexOf(notification_id);
     if (index !== -1) notification_settings.splice(index);                                     // remove notification option from list
+    updateBackend();
     return await AsyncStorage.setItem('notification_settings', JSON.stringify(notification_settings));
 }
 
 export async function isBeingNotified(notification_id) {
-    notification_settings = JSON.parse(await AsyncStorage.getItem('notification_settings') || "[]");
+    notification_settings = getNotificationSettings();
     var index = notification_settings.indexOf(notification_id);
     if (index !== -1) return true;
     else return false;
 }
 
+async function updateBackend() {
+    fetch('https://www.stanforddaily.com/wp-json/tsd/v1/push-notification/users/' + await getToken(), {
+        method: 'PUT',
+        headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            subscribing: {
+                list: getNotificationSettings(),
+                category_ids: getCategoriesFollowed(),
+                author_ids: getAuthorsFollowed(),
+                location_ids: getLocationsFollowed()
+            },
+        }),
+    });
+}
