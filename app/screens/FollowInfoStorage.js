6e68d02ab7361606233701397854317a9b0b8514
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-03-06 10:37:41 -0800

    Update app/screens/FollowInfoStorage.js
    Co-Authored-By: aotsai <aotsai@stanford.edu>

--- app/screens/FollowInfoStorage.js
+++ app/screens/FollowInfoStorage.js
@@ -93,7 +93,7 @@ export async function isFollowingLocation(location_id) {
 export async function addNotificationSetting(notification_id) {
     notification_settings = getNotificationSettings();  
     notification_settings.push(notification_id);                                                        
-    updateBackend();
+    await updateBackend();
     return await AsyncStorage.setItem('notification_settings', JSON.stringify(notification_settings));
 }
 
