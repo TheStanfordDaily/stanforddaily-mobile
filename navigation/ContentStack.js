53135c80d1e5a8247e5072e9470dfc9370f3ff40
Matthew Turk <mjturk15@gmail.com>
2022-04-14 23:04:54 -0700

    Search button takes you to a new view.

--- navigation/ContentStack.js
+++ navigation/ContentStack.js
@@ -6,24 +6,27 @@ import Search from '../screens/Search';
 import Post from '../screens/Post';
 import Author from '../screens/Author';
 import Category from '../screens/Category';
+import Icon from '@expo/vector-icons/Ionicons';
+import { TouchableWithoutFeedback, TouchableNativeFeedback, TouchableOpacity } from 'react-native-gesture-handler';
+import { Strings } from '../constants'
 
 const Stack = createNativeStackNavigator();
 
 export default function ContentStack() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={Home} options={{headerTitle: () => (<Image style={{ width: 260, height: 30 }} source={require("../assets/media/DailyLogoCardinal.png")} />)}} />
-      {/* <Stack.Screen name="Search" component={Search} /> */}
+      <Stack.Screen name="Home" component={Home} options={({ route, navigation }) => ({
+        headerTitle: () => (<Image style={{ width: 260, height: 30 }} source={require("../assets/media/DailyLogoCardinal.png")} />),
+        headerRight: () => (<TouchableOpacity onPress={() => navigation.navigate(Strings.search)}><Icon name={(Platform.OS === 'ios' ? 'ios' : 'md') + "-search"} size={25} color="#000" /></TouchableOpacity>)
+      })}/>
+      <Stack.Screen name="Search" component={Search} />
       <Stack.Screen name="Post" component={Post} options={({ route }) => ({
         headerBackTitle: "",
         headerTitle: Platform.OS === 'ios' ? '' : () => (<Image style={{ width: 260, height: 30 }} source={require("../assets/media/DailyLogoCardinal.png")} />),
         // headerShown: false,
         headerTransparent: Platform.OS === "ios",
         headerTintColor: Platform.OS === "ios" ? "#fff" : "#000",
-        // headerTitle: false
-        // headerTitle: () => (<Image style={{ width: 260, height: 30 }} source={require("../assets/media/DailyLogoWhite.png")} />),
       })}/>
-      {/* <Stack.Screen name="Category" component={Settings} /> */}
       <Stack.Screen name="Category" component={Category} />
       <Stack.Screen name="Author" component={Author} />
     </Stack.Navigator>
---
+++ screens/Search.js
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import { Image, Text, Platform } from 'react-native'
+
+export default function Search(props) {
+    return <Text>Hello</Text>
+}
