fe7336010d5621cad023d75ea0c00585cd596cff
Matthew Turk <mjturk15@gmail.com>
2022-11-12 17:45:42 -0800

    Minor refactoring.

--- App.js
+++ App.js
@@ -31,7 +31,7 @@ Notifications.setNotificationHandler({
   handleNotification: async () => ({
     shouldShowAlert: true,
     shouldPlaySound: false,
-    shouldSetBadge: false,
+    shouldSetBadge: false
   })
 })
 
