c0e6a15100f51bf834396b707f066c654be9b84d
Matthew Turk <mjturk15@gmail.com>
2023-01-16 10:23:06 -0800

    Minor refactoring

--- App.js
+++ App.js
@@ -178,7 +178,7 @@ export default function App() {
 
     // Handles any event in which appearance preferences change.
     Appearance.addChangeListener(listener => {
-        setTheme(listener.colorScheme)
+      setTheme(listener.colorScheme)
       // TODO: Add return function for removing listener when user opts out of automatic theme changes.
     })    
 
