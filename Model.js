f585973e6918212f1c82bd12be17600429ce661f
Matthew Turk <mjturk15@gmail.com>
2023-03-24 18:47:35 -0700

    Home page filtering

--- Model.js
+++ Model.js
@@ -14,33 +14,50 @@ export const useWordPress = (pageNumber = 1) => {
       // Set initial loading state.
       setLoading(true)
   
-      Object.values(Sections).forEach(async category => {
-        try {
-          const response = await wp.posts().categories(category.id).perPage(6).page(pageNumber).get()
-          setData((prevState) => ({
-            ...prevState,
-            [pageNumber === 1 ? category.slug : "wildcard"]: [...(category.slug in prevState ? prevState[category.slug] : []), ...response]
-          }))
+      if (pageNumber === 1) {
+        Object.values(Sections).forEach(async category => {
+          try {
+            const response = await wp.posts().categories(category.id).perPage(6).page(pageNumber).get()
+            setData((prevState) => ({
+              ...prevState,
+              [category.slug]: [...(category.slug in prevState ? prevState[category.slug] : []), ...response]
+            }))
 
-          if (articles?.culture?.length < 4 && category.slug === Sections.ARTS_LIFE.slug || category.slug === Sections.THE_GRIND.slug) {
-            setArticles((prevState) => ({
-              ...prevState,
-              culture: [...(prevState?.culture ?? []), ...response]
-            }))
+            if (articles?.culture?.length < 4 && category.slug === Sections.ARTS_LIFE.slug || category.slug === Sections.THE_GRIND.slug) {
+              setArticles((prevState) => ({
+                ...prevState,
+                culture: [...(prevState?.culture ?? []), ...response]
+              }))
+            }
+            
+            if (pageNumber === 1) {
+              setArticles((prevState) => ({
+                ...prevState,
+                [category.slug]: response.filter(item => !item.categories.includes(Sections.FEATURED.id) || category.slug === Sections.FEATURED.slug)
+              }))
+            }
+          } catch (error) {
+            setError(error)
+          } finally {
+            setLoading(false)
           }
-          
-          if (pageNumber === 1) {
-            setArticles((prevState) => ({
+        })
+      } else {
+        try {
+          wp.posts().perPage(12).page(pageNumber).get().then(posts => {
+            setData((prevState) => ({
               ...prevState,
-              [category.slug]: response.filter(item => !item.categories.includes(Sections.FEATURED.id) || category.slug === Sections.FEATURED.slug)
+              wildcard: [...(prevState?.wildcard ?? []), ...posts.filter(post => {
+                return !Object.keys(articles).some(category => articles[category].some(item => item.id === post.id))
+              })]
             }))
-          }
+          })
         } catch (error) {
           setError(error)
         } finally {
           setLoading(false)
         }
-      })
+      } 
     }, [pageNumber])
     
     return { data, articles, loading, error }
