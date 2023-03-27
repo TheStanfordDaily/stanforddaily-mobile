c15d0f6382c12ca82e1e4160201b455329a9d854
Matthew Turk <mjturk15@gmail.com>
2023-03-27 13:48:59 -0700

    Still less than ideal

--- components/common/SearchBar.js
+++ components/common/SearchBar.js
@@ -1,54 +1,52 @@
 import React from 'react';
-import { StyleSheet, Dimensions, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Dimensions, View } from 'react-native';
 import { Icon, Input, Button, useTheme } from '@ui-kitten/components';
 import { Spacing } from '../../utils/constants';
-import { ThemeContext } from '../../theme-context';
 
 const { width } = Dimensions.get("window");
 
 export default function SearchBar({ searchQuery, onChangeText, onSearch, onClose }) {
-    const theme = useTheme();
-    
-    return (
-        <View style={{
-            flex: 1,
-            width,
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "space-between",
-            backgroundColor: theme === "dark" ? "#1E1E1E" : "white",
-            paddingRight: Spacing.medium,
-        }}>
-            <Input
-                autoFocus
-                style={{
-                    flex: 1,
-                    backgroundColor: "white",
-                    borderRadius: 5,
-                    // paddingHorizontal: Spacing.medium,
-                    // marginRight: Spacing.medium,
-                }}
-                value={searchQuery}
-                onChangeText={onChangeText}
-                onSubmitEditing={onSearch}
-                placeholderTextColor={theme === "dark" ? "white" : "black"}
-                placeholder="Search for articles or topics"
-                returnKeyType="search"
-                onTouchCancel={onClose}
-                clearButtonMode="while-editing"
-            />
-            <Button onPress={onClose} appearance="ghost">Cancel</Button>
-        </View>
-    );
+  const theme = useTheme();
+  
+  return (
+    <View style={{...styles.container, backgroundColor: theme === "dark" ? "#1E1E1E" : "white"}}>
+      <Input
+        autoFocus
+        style={styles.searchBar}
+        value={searchQuery}
+        onChangeText={onChangeText}
+        onSubmitEditing={onSearch}
+        placeholderTextColor={theme === "dark" ? "white" : "black"}
+        placeholder="Search for articles or topics"
+        returnKeyType="search"
+        onTouchCancel={onClose}
+        clearButtonMode="while-editing"
+        textStyle={styles.inputText}
+      />
+      <Button onPress={onClose} appearance="ghost">Cancel</Button>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    searchBar: {
-        paddingHorizontal: Spacing.medium,
-        paddingVertical: Spacing.small,
-    },
-    icon: {
-        width: 32,
-        height: 32,
-    },
-});
+  container: {
+    flex: 1,
+    width,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingRight: Spacing.medium
+  },
+  searchBar: {
+    flex: 1,
+    backgroundColor: "white",
+    borderRadius: 5
+  },
+  inputText: {
+    marginHorizontal: 0
+  },
+  icon: {
+    width: 32,
+    height: 32
+  }
+})
