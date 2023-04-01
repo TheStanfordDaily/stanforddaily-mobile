1aaba3d83b362aa320adfd0cdc666eee5bc827bc
Matthew Turk <mjturk15@gmail.com>
2023-04-01 16:01:26 -0700

    Indentation

--- components/common/Diptych.js
+++ components/common/Diptych.js
@@ -13,75 +13,75 @@ const { width, height } = Dimensions.get("window")
 const pixelRatio = PixelRatio.get()
 
 export default function Diptych(props) {
-    const articles = props.articles.length % 2 === 0 ? props.articles : props.articles.slice(0, -1)
-    const [selection, setSelection] = useState(0)
-    const { deviceType } = useContext(ThemeContext)
-    const groupSize  = deviceType === Device.DeviceType.PHONE ? 2 : 3
-    
-    const Header = (props) => (
-      <React.Fragment>
-        <Image source={{ uri: `${props.source}?w=${pixelRatio*width/2}` }} style={{ flex: 1 }} />
-      </React.Fragment>
-    )
+  const articles = props.articles.length % 2 === 0 ? props.articles : props.articles.slice(0, -1)
+  const [selection, setSelection] = useState(0)
+  const { deviceType } = useContext(ThemeContext)
+  const groupSize  = deviceType === Device.DeviceType.PHONE ? 2 : 3
+  
+  const Header = (props) => (
+    <React.Fragment>
+      <Image source={{ uri: `${props.source}?w=${pixelRatio*width/2}` }} style={{ flex: 1 }} />
+    </React.Fragment>
+  )
 
-    const Footer = (props) => (
-      <View style={styles.headerTextContainer}>
-        <Text category="label">{moment(new Date(props.date)).fromNow().toUpperCase()}</Text>
-      </View>
-    )
+  const Footer = (props) => (
+    <View style={styles.headerTextContainer}>
+      <Text category="label">{moment(new Date(props.date)).fromNow().toUpperCase()}</Text>
+    </View>
+  )
 
-    return (
-        <PagerView peekEnabled style={styles.container} initialPage={0} onPageSelected={e => setSelection(e.nativeEvent.position)} overdrag>
-            {_.chunk(articles, groupSize).map((couplet, index) => (
-              <View collapsable={false} style={{ flex: 1, flexDirection: "row" }} key={index}>
-                {couplet.map((item, _index) => (
-                  <Card
-                    style={styles.card}
-                    key={_index}
-                    header={<Header source={item["jetpack_featured_media_url"]}/>}
-                    footer={<Footer date={item["date"]}/>}
-                    {...{...props, onPress: () => props.navigation.navigate("Post", { article: item })}}>
-                    <Text category={"p1"}>{decode(item.title.rendered)}</Text>
-                  </Card>
-                ))}
-              </View>
-            ))}
-        </PagerView>
-    )
+  return (
+    <PagerView peekEnabled style={styles.container} initialPage={0} onPageSelected={e => setSelection(e.nativeEvent.position)} overdrag>
+      {_.chunk(articles, groupSize).map((couplet, index) => (
+        <View collapsable={false} style={{ flex: 1, flexDirection: "row" }} key={index}>
+          {couplet.map((item, _index) => (
+            <Card
+              style={styles.card}
+              key={_index}
+              header={<Header source={item["jetpack_featured_media_url"]}/>}
+              footer={<Footer date={item["date"]}/>}
+              {...{...props, onPress: () => props.navigation.navigate("Post", { article: item })}}>
+              <Text category="p1">{decode(item.title.rendered)}</Text>
+            </Card>
+          ))}
+        </View>
+      ))}
+    </PagerView>
+  )
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      height: 275, // Maybe use PixelRatio here?
-      paddingHorizontal: Spacing.medium,
-      paddingVertical: 4
-    },
-    tab: {
-      height: 192,
-      alignItems: "center",
-      justifyContent: "center"
-    },
-    card: {
-      flex: 1,
-      height: 275, // Maybe use PixelRatio here?
-      marginHorizontal: 5
-    },
-    headerTextContainer: {
-      paddingHorizontal: 20,
-      paddingVertical: 10
-    },
-    headerImage: {
-      width: 300,
-      height: 200
-    },
-    footerContainer: {
-      flexDirection: "row",
-      justifyContent: "flex-end",
-      paddingHorizontal: 20,
-      paddingVertical: 20
-    },
-    footerControl: {
-      marginHorizontal: 4
-    }
+  container: {
+    flex: 1,
+    height: 275, // Maybe use PixelRatio here?
+    paddingHorizontal: Spacing.medium,
+    paddingVertical: 4
+  },
+  tab: {
+    height: 192,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  card: {
+    flex: 1,
+    height: 275, // Maybe use PixelRatio here?
+    marginHorizontal: 5
+  },
+  headerTextContainer: {
+    paddingHorizontal: 20,
+    paddingVertical: 10
+  },
+  headerImage: {
+    width: 300,
+    height: 200
+  },
+  footerContainer: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    paddingHorizontal: 20,
+    paddingVertical: 20
+  },
+  footerControl: {
+    marginHorizontal: 4
+  }
 })
