824cd7ea92b6160022cbe97ac6949ff12a8568d2
Matthew Turk <mjturk15@gmail.com>
2023-12-31 16:46:14 -0600

    `PagerView` performance optimizations

--- components/screens/Section.jsx
+++ components/screens/Section.jsx
@@ -1,16 +1,16 @@
-import { Layout, List, Tab, TabBar, Text } from "@ui-kitten/components";
+import { Layout, List, Tab, TabBar } from "@ui-kitten/components";
 import { DeviceType } from "expo-device";
-import React, { useCallback, useContext, useEffect, useState } from "react";
+import React, { useCallback, useContext, useEffect, useState, useRef, useMemo } from "react";
 import { ActivityIndicator, ScrollView, StyleSheet, View } from "react-native";
 import PagerView from "react-native-pager-view";
 
 import { ThemeContext } from "../../theme-context";
 import { Spacing } from "../../utils/constants";
-import { throttle } from "../../utils/format";
 import Model from "../../utils/model";
 import Wildcard from "../common/Wildcard";
 
 const BATCH_SIZE = 16;
+const MemoizedWildcard = React.memo(Wildcard);
 
 /**
  * The `Section` screen displays a list of articles from a specific category.
@@ -28,151 +28,147 @@ const BATCH_SIZE = 16;
  * @property {Object} navigation From React Navigation.
  * @exports Section
  */
-export default function Section({ route, navigation }) {
+function Section({ route, navigation }) {
   const { category, seed } = route.params;
-  // const [articlesLoading, setArticlesLoading] = useState(false);
   const [selection, setSelection] = useState(0);
   const [allArticles, setAllArticles] = useState(seed);
-  // The `theme` and `deviceType` are used to determine the number of columns in the list of articles.
+  const [pageScrollState, setPageScrollState] = useState("idle");
+
   const { theme, deviceType } = useContext(ThemeContext);
   const columnCount = deviceType === DeviceType.PHONE ? 1 : 2;
-
   const Container = theme === "dark" ? Layout : View;
-  const pagerViewRef = React.useRef(null);
+  const pagerViewRef = useRef(null);
 
-  // Might try memoizing this down the line.
+  const fetchResults = async (subcategory, pageNumber) => {
+    try {
+      const posts = await Model.posts().categories(subcategory.id).perPage(BATCH_SIZE).page(pageNumber).get();
+      return posts;
+    } catch (error) {
+      console.error(error);
+      return [];
+    }
+  };
 
   /**
    * @component
    * @property {Array<import("../../utils/model").WordPressPost>} sectionArticles
    * @property {number} basePageCount Before loading more articles, we calculate the number of pages we can skip when calling the API.
-   * @property {number} sectionId
    * @property {import("../../utils/constants").Category} subcategory
    */
-  const CategoryContainer = React.memo(({ sectionArticles, basePageCount, subcategory }) => {
-    const [possiblyReachedEnd, setPossiblyReachedEnd] = useState(false);
-    const [articlesLoading, setArticlesLoading] = useState(false);
-    const [articles, setArticles] = useState(sectionArticles);
-    // Since there might already be a chronology of several articles passed in from the seed data, there's no need to fetch them again.
-    // Right now the page numbers are a little out of sync, so that needs to be fixed ASAP.
-    const [pageNumber, setPageNumber] = useState(basePageCount);
+  const CategoryContainer = useMemo(
+    () =>
+      React.memo(({ sectionArticles, basePageCount, subcategory }) => {
+        const [articles, setArticles] = useState(sectionArticles);
+        const [pageNumber, setPageNumber] = useState(basePageCount);
+        const [articlesLoading, setArticlesLoading] = useState(false);
+        const [possiblyReachedEnd, setPossiblyReachedEnd] = useState(false);
 
-    const fetchResults = async () => {
-      setArticlesLoading(true);
-      try {
-        const posts = await Model.posts()
-          .categories(subcategory.id)
-          .perPage(BATCH_SIZE)
-          .page(basePageCount + pageNumber)
-          .get();
-        // TODO: Update the "all" list of articles, too.
-        setArticles((prev) => [...prev, ...posts]);
-      } catch (error) {
-        console.log(error);
-        if (error.data?.status === 400) {
-          setPossiblyReachedEnd(true);
-        }
-      } finally {
-        setArticlesLoading(false);
-        // setAllArticles((prev) => [...prev, ...articles.filter((item) => !prev.find((article) => article.id === item.id))]);
-      }
-    };
+        const loadMoreArticles = async () => {
+          if (possiblyReachedEnd || articlesLoading) return;
 
-    const throttledFetch = useCallback(throttle(fetchResults, 3000), []);
+          setArticlesLoading(true);
+          const newArticles = await fetchResults(subcategory, pageNumber + 1);
+          if (newArticles.length === 0) {
+            setPossiblyReachedEnd(true);
+          } else {
+            setArticles((prev) => [...prev, ...newArticles]);
+            setPageNumber((prev) => prev + 1);
+          }
+          setArticlesLoading(false);
+        };
 
-    useEffect(() => {
-      throttledFetch();
-    }, [pageNumber]);
+        return (
+          <Container>
+            <List
+              data={articles}
+              style={{ backgroundColor: "transparent" }}
+              numColumns={columnCount}
+              scrollEventThrottle={BATCH_SIZE}
+              showsVerticalScrollIndicator={false}
+              onEndReached={loadMoreArticles}
+              onEndReachedThreshold={0.5}
+              renderItem={({ item, index }) => (
+                <MemoizedWildcard
+                  key={`${subcategory.id}-${item.id}`}
+                  item={item}
+                  index={index}
+                  navigation={navigation}
+                />
+              )}
+              ListFooterComponent={() => articlesLoading && <ActivityIndicator />}
+            />
+          </Container>
+        );
+      }),
+    [theme, deviceType, navigation]
+  );
 
-    return (
-      <Container>
-        <List
-          data={articles}
-          style={{ backgroundColor: "transparent" }}
-          numColumns={columnCount}
-          scrollEventThrottle={BATCH_SIZE}
-          showsVerticalScrollIndicator={false}
-          onEndReachedThreshold={1}
-          onEndReached={() => {
-            if (!possiblyReachedEnd && !articlesLoading) {
-              setPageNumber(pageNumber + 1);
-            }
-          }}
-          renderItem={({ item, index }) => (
-            <Wildcard key={`${subcategory.id}-${item.id}`} item={item} index={index} navigation={navigation} verbose />
-          )}
-          ListFooterComponent={() => {
-            // if (!possiblyReachedEnd || articlesLoading) {
-            //   return <ActivityIndicator />;
-            // }
-          }}
+  const renderTabs = () => {
+    const tabs = [
+      { name: `All ${category.name}`, id: "all" },
+      ...Object.values(category.desks ?? {}).map((desk, index) => ({ ...desk, id: index + 1 })),
+    ];
+
+    return tabs.map((tab, index) => (
+      <Tab
+        key={tab.id}
+        onSelect={() => {
+          setSelection(index);
+          pagerViewRef.current?.setPage(index);
+        }}
+        selected={selection === index}
+        style={styles.tab}
+        title={tab.name}
+      />
+    ));
+  };
+
+  const renderPagerView = () => {
+    const sections = [{ ...category, id: "all" }, ...Object.values(category.desks ?? {})];
+
+    return sections.map((section, index) => {
+      const numericId = section.id === "all" ? category.id : section.id;
+      const subcategory = section.id === "all" ? category : category.desks?.[section.id] ?? category;
+      const filteredSeed = seed.filter((item) => item.categories.includes(numericId));
+      console.log(category.desks, section.id);
+
+      return (
+        <CategoryContainer
+          key={section.id}
+          sectionArticles={filteredSeed}
+          basePageCount={Math.max(1, Math.floor(filteredSeed.length / BATCH_SIZE) - 1)}
+          subcategory={subcategory}
         />
-      </Container>
-    );
-  });
+      );
+    });
+  };
 
   return (
     <>
       {category.desks && (
-        // TODO: Make sure this is accessible using VoiceOver.
         <TabBar
           selectedIndex={selection}
           onSelect={(index) => {
             setSelection(index);
-            pagerViewRef.current.setPage(index);
+            pagerViewRef.current?.setPage(index);
           }}
           indicatorStyle={styles.indicator}
         >
           <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-            <Tab
-              onSelect={() => {
-                setSelection(0);
-                pagerViewRef.current.setPage(0);
-              }}
-              selected={selection === 0}
-              style={styles.tab}
-              title={`All ${category.name}`}
-            />
-            {Object.values(category.desks).map((section, index) => (
-              <Tab
-                onSelect={() => {
-                  setSelection(index + 1);
-                  pagerViewRef.current.setPage(index + 1);
-                }}
-                selected={selection === index + 1}
-                style={styles.tab}
-                title={section.name}
-                key={index}
-              />
-            ))}
+            {renderTabs()}
           </ScrollView>
         </TabBar>
       )}
       <PagerView
         ref={pagerViewRef}
-        onPageSelected={(e) => {
-          setSelection(e.nativeEvent.position);
-        }}
-        scrollEnabled={Object.keys(category.desks ?? {}).length > 0} // Disable swiping in `PagerView` if there are no desks.
+        onPageSelected={(e) => setSelection(e.nativeEvent.position)}
+        onPageScrollStateChanged={(e) => setPageScrollState(e.nativeEvent.pageScrollState)}
+        scrollEnabled={Object.keys(category.desks ?? {}).length > 0}
         style={styles.container}
         initialPage={0}
         overdrag
       >
-        {Object.entries({ ...{ category }, ...(category.desks ?? {}) }).map(([sectionId, sectionArticles], index) => {
-          const numericId = category.desks ? (category.desks[sectionId] ?? category).id : category.id;
-          const subcategory = category.desks?.[sectionId] ?? category;
-          const filteredSeed = seed.filter((item) => item.categories.includes(numericId));
-
-          return (
-            <CategoryContainer
-              key={index}
-              sectionArticles={filteredSeed}
-              basePageCount={Math.max(1, Math.floor(filteredSeed.length / BATCH_SIZE) - 1)}
-              sectionId={sectionId}
-              subcategory={subcategory}
-            />
-          );
-        })}
+        {renderPagerView()}
       </PagerView>
     </>
   );
@@ -182,15 +178,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  empty: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+  tab: {
+    marginHorizontal: Spacing.medium,
   },
   indicator: {
     opacity: 0,
   },
-  tab: {
-    marginHorizontal: Spacing.medium,
-  },
 });
+
+export default Section;
